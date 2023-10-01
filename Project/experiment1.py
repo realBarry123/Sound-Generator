7805a641429b95e4b67f7757cf3d0872323af37a
realBarry123 <77864928+realBarry123@users.noreply.github.com>
2023-10-01 10:01:32 -0700

    Add combining parts

--- Project/experiment1.py
+++ Project/experiment1.py
@@ -7,6 +7,8 @@ chords = [[261.63, 327.04],[196, 245],[220, 264],[174.61, 209.53]]
 
 FS = 44100
 
+# chords
+
 signal = []
 
 for chord in chords:
@@ -15,19 +17,22 @@ for chord in chords:
         signal.append(note)
 
 signal = np.concatenate(signal)
-wavfile.write(str("melody.wav"), FS, signal)
+combined = signal * 0.06 / np.max(signal)
 
-signal = []
+# melody
+for i in range(2):
+    signal = []
 
-for chord in chords:
-    for i in range(4):
-        note = create_signal(random.choice(chord) * random.choice((1, 2, 1.5)), 1.25, random.choice(("sin", "triangle")))
-        signal.append(note)
+    for chord in chords:
+        for j in range(4):
+            note = create_signal(random.choice(chord) * random.choice((1, 2, 1.5)), 1.25, "triangle")
+            signal.append(note)
 
-signal = np.concatenate(signal)
+    signal = np.concatenate(signal)
 
-fileName = input("Name your audio: ")
-wavfile.write(str("chords.wav"), FS, signal)
+    combined += signal * 0.1 / np.max(signal)
+
+# bass
 
 signal = []
 
@@ -38,5 +43,6 @@ for chord in chords:
 
 signal = np.concatenate(signal)
 
-fileName = input("Name your audio: ")
-wavfile.write(str("bass.wav"), FS, signal)
+combined += signal * 0.1 / np.max(signal)
+
+wavfile.write(str("combined.wav"), FS, combined)
